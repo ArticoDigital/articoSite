e414c413ba69065564c4724d2efdd922e02d6a98
santo52 <sanruiz1003@gmail.com>
2016-06-26 18:49:12 -0500

    Controllers

---
+++ app/Http/Controllers/PositionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Artico\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Artico\Http\Requests;
+
+class PositionController extends Controller
+{
+    function index(){
+        return "vista positions";
+    }
+}
