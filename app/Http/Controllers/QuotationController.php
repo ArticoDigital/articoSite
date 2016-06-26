65b54607ce916f251d42c0d24ba3cbaeb85e3917
santo52 <sanruiz1003@gmail.com>
2016-06-26 18:44:54 -0500

    Quotation controller

---
+++ app/Http/Controllers/QuotationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Artico\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Artico\Http\Requests;
+
+class QuotationController extends Controller
+{
+    function index(){
+        return "vista paisa";
+    }
+}
